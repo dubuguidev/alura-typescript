7a068f5836f94bf897a7f6c7bca86613db2137fc
Dubugui <gabrielbcostadev@gmail.com>
2025-09-08 11:14:42 -0300

    aula 1,2,3,4,5

---
+++ bytebank.js
@@ -0,0 +1,41 @@
+let saldo = 3000;
+
+const elementoSaldo = document.querySelector(".saldo-valor .valor");
+elementoSaldo.textContent = saldo;
+
+const elementoFormulario = document.querySelector(".block-nova-transacao form");
+elementoFormulario.addEventListener("submit", function(event){
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert("Por favor, preencha todos os cmapos da transação");
+        return;
+    }    
+
+    const inputTipoTrasacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+
+    let tipoTransacao = inputTipoTrasacao.value;
+    let valor = inputValor.value;
+    let data = inputData.value;
+
+    if(tipoTransacao == "Depósito") {
+        saldo += valor;
+    } else if (tipoTransacao == "Transferência" || tipoTransacao == "Pagamento de Boleto") {
+        saldo -= valor;
+    } else {
+        alert("Tipo de Transação é inválida")
+        return;
+    }
+
+    elementoSaldo.textContent = saldo;
+
+    const novaTransacao = {
+        tipoTransacao: tipoTransacao,
+        valor: valor,
+        data: data
+    }
+
+    console.log(novaTransacao);
+    elementoFormulario.reset ();
+})
